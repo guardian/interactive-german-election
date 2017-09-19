5000ffa4cb920dc1724e5a3569d74e4005f96beb
seanclarkeguardian <sean.clarke@guardian.co.uk>
2017-09-19 11:17:34 +0100

    style changes for table at smaller widths

--- src/prepmaps.js
+++ src/prepmaps.js
@@ -18,25 +18,25 @@ function cleannumber(input) {
     }
 }
 
- function cleanup(constituencies) {
-     constituencies.forEach(function(constituency){
-    switch (constituency.constituencyWinner) {
-        case '2' : constituency.constituencyWinnerParty = "CDU";
-        break;
-        case '1' : constituency.constituencyWinnerParty = "SPD";
-        break;
-        case '34' : constituency.constituencyWinnerParty = "linke";
-        break;
-        case '5' : constituency.constituencyWinnerParty = "greens";
-        break;
-        case '3' : constituency.constituencyWinnerParty = "CSU";
-        break;
-        default : "undeclared"
-    }
+function cleanup(constituencies) {
+    constituencies.forEach(function (constituency) {
+        switch (constituency.constituencyWinner) {
+            case '2': constituency.constituencyWinnerParty = "CDU";
+                break;
+            case '1': constituency.constituencyWinnerParty = "SPD";
+                break;
+            case '34': constituency.constituencyWinnerParty = "linke";
+                break;
+            case '5': constituency.constituencyWinnerParty = "greens";
+                break;
+            case '3': constituency.constituencyWinnerParty = "CSU";
+                break;
+            default: "undeclared"
+        }
     })
-} 
+}
 
-export default async function prepmaps(seats,wk_names) {
+export default async function prepmaps(seats, wk_names) {
     cleanup(seats);
     var $ = cheerio.load(maptemplate);
     var wks = Array.from($('path'));
@@ -59,16 +59,16 @@ export default async function prepmaps(seats,wk_names) {
             $(w).removeClass('gv-undeclared')
         }
     })
-     // Add class for SPD share
-     wks.forEach(function (w) {
+    // Add class for SPD share
+    wks.forEach(function (w) {
         var result = seats.find(function (r) {
             return cleannumber(r.id) == $(w).attr('id')
         });
-        if (result != undefined){
-        var spdvote = result.zweitstimmen.find(function(z){
-            return z.party == 'SPD';
-        })
-        var spdshare = spdvote.percent;
+        if (result != undefined) {
+            var spdvote = result.zweitstimmen.find(function (z) {
+                return z.party == 'SPD';
+            })
+            var spdshare = spdvote.percent;
 
             $(w).attr('data-spdshare', `${spdshare}`)
 
@@ -84,18 +84,47 @@ export default async function prepmaps(seats,wk_names) {
 
         }
 
-    })  
-     //Add class for AfD share
- wks.forEach(function (w) {
+    })
+
+    // Add class for SPD change
+    wks.forEach(function (w) {
         var result = seats.find(function (r) {
             return cleannumber(r.id) == $(w).attr('id')
         });
-      
-        if (result != undefined){
-        var afdvote = result.zweitstimmen.find(function(z){
-            return z.party == 'AfD';
-        })
-        var afdshare = afdvote.percent;
+        if (result != undefined) {
+            var spdvote = result.zweitstimmen.find(function (z) {
+                return z.party == 'SPD';
+            })
+            var spdchange = spdvote.percentChange;
+
+            $(w).attr('data-spdchange', `${spdchange}`)
+
+            if (spdchange <= -20) {
+                $(w).addClass('gv-spd-change-band-4');
+            } else if (spdchange < -20 && spdchange <= -10) {
+                $(w).addClass('gv-spd-change-band-3');
+            } else if (spdchange < -10 && spdchange <= -5) {
+                $(w).addClass('gv-spd-change-band-2');
+            } else {
+                $(w).addClass('gv-spd-change-band-1')
+            }
+
+        }
+
+    })
+
+
+    //Add class for AfD share
+    wks.forEach(function (w) {
+        var result = seats.find(function (r) {
+            return cleannumber(r.id) == $(w).attr('id')
+        });
+
+        if (result != undefined) {
+            var afdvote = result.zweitstimmen.find(function (z) {
+                return z.party == 'AfD';
+            })
+            var afdshare = afdvote.percent;
 
             $(w).attr('data-afdshare', `${afdshare}`)
 
@@ -111,37 +140,37 @@ export default async function prepmaps(seats,wk_names) {
 
         }
 
-    })  
-
-
-
-// Add class for CDU/CSU share
- wks.forEach(function (w) {
-    var result = seats.find(function (r) {
-        return cleannumber(r.id) == $(w).attr('id')
-    });
-  
-    if (result != undefined){
-    var cduvote = result.zweitstimmen.find(function(z){
-        return z.party == 'CDU' || z.party == 'CSU';
     })
-    var cdushare = cduvote.percent;
 
-        $(w).attr('data-cdushare', `${cdushare}`)
 
-        if (cdushare >= 60) {
-            $(w).addClass('gv-cdu-band-4');
-        } else if (cdushare < 60 && cdushare >= 50) {
-            $(w).addClass('gv-cdu-band-3');
-        } else if (cdushare < 50 && cdushare >= 40) {
-            $(w).addClass('gv-cdu-band-2');
-        } else {
-            $(w).addClass('gv-cdu-band-1')
+
+    // Add class for CDU/CSU share
+    wks.forEach(function (w) {
+        var result = seats.find(function (r) {
+            return cleannumber(r.id) == $(w).attr('id')
+        });
+
+        if (result != undefined) {
+            var cduvote = result.zweitstimmen.find(function (z) {
+                return z.party == 'CDU' || z.party == 'CSU';
+            })
+            var cdushare = cduvote.percent;
+
+            $(w).attr('data-cdushare', `${cdushare}`)
+
+            if (cdushare >= 60) {
+                $(w).addClass('gv-cdu-band-4');
+            } else if (cdushare < 60 && cdushare >= 50) {
+                $(w).addClass('gv-cdu-band-3');
+            } else if (cdushare < 50 && cdushare >= 40) {
+                $(w).addClass('gv-cdu-band-2');
+            } else {
+                $(w).addClass('gv-cdu-band-1')
+            }
+
         }
 
-    }
-
-})  
+    })
 
 
     /* commenting out population density stuff
